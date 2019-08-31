eff8a8503ab56d45b4a0ba7508715367e4368471
Ari Carvalho <ari-carvalho@hotmail.com>
2019-08-31 19:34:57 -0300

    HospedagemEmProcessoComum

---
+++ HospedagemEmProcessoComum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+using System;
+
+namespace GenericWebHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("http://localhost:5000/");
+
+            using (var host = WebHost.Start(app => app.Response.WriteAsync("Hello, World!")))
+            {
+                Console.WriteLine("Use Ctrl-C to shutdown the host...");
+                host.WaitForShutdown();
+            }
+        }
+
+
+    }
+}
---
+++ SiteUtilizandoAspNetMvc4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SiteUtilizandoAspNetMvc4.MvcApplication" Language="C#" %>
