b3d9bcfacefb2f78ad55de0aba29e4718a4010f9
Ari Carvalho <ari-carvalho@hotmail.com>
2019-09-09 07:37:10 -0300

    201909090737

---
+++ ArquivosPDFUtilizandoItext/Program.cs
@@ -0,0 +1,24 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.IO;
+
+namespace ArquivosPDFUtilizandoItext
+{
+    class Program
+    {
+    
+            /// <exception cref="System.IO.IOException"/>
+            public static void Main(String[] args)
+            {
+                using(var file =new FileStream("HelloWorld.pdf", FileMode.OpenOrCreate))
+                using (var document = new Document(new Rectangle(200, 100)))
+                {
+                    var writer = PdfWriter.GetInstance(document, file);
+                    document.Open();
+                    document.Add(new Paragraph("Hello World!"));
+                }
+              
+            }
+        }
+    }   
